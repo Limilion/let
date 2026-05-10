ef83ab22fece7b99bf2a212da33f40669876bdcb
Limilion <Limilion@users.noreply.github.com>
2026-05-10 13:08:47 +0000

    deploy: dc0b8014cf7df871d2c86dacf8a884430aa80e42

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2582829907" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1268713425" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
